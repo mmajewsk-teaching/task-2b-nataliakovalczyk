84ed1cf269c511a6bf5632adbe30e3eac5c0920d
nataliakovalczyk <nkowalczyk@student.agh.edu.pl>
2025-11-26 15:27:55 +0100

    task 2 corrected

--- task.py
+++ task.py
@@ -28,114 +28,84 @@
 #
 #Delete these comments before commit!
 #Good luck.
+import json
+import random
+from typing import List, Dict, Any
 
-class Student:
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.grades = {}
-        self.attendance = {}
-    
-    def add_grade(self, subject, grade):
-        if subject not in self.grades:
-            self.grades[subject] = []
-        self.grades[subject].append(grade)
+Grades = Dict[str, List[int]]
+Attendance = Dict[str, List[int]]
+Student = Dict[str, Any]
+Dataset = Dict[str, Any]
+
+def load_data(file_path: str) -> Dict:
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+def save_data(file_path: str, data: Dict):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=2)
+
+def student_couse_average(student: Dict, course: str) -> float:
+    grades: List[int] = student['grades'].get(course, [])
+    return sum(grades) / len(grades) if grades else 0.0
+
+def student_total_average(student: Dict) -> float:
+    all_grades: List[int] = [grade for courses in student['grades'].values() for grade in courses]
+    return sum(all_grades) / len(all_grades) if all_grades else 0.0
+
+def student_attendance(student: Dict, course: str) -> float:
+    attendance: List[int] = student.get('attendance', {}).get(course, [])
+    return sum(attendance) / len(attendance) if attendance else 0.0
+
+def course_average(data: Dict, course: str) -> float:
+    grades: List[int] = []
+    for student in data['students']:
+        grades.extend(student['grades'].get(course, []))
+    return sum(grades) / len(grades) if grades else 0.0
+
+def course_attendance(data: Dataset, course: str) -> float:
+    attendance: List[int] = []
+    for student in data["students"]:
+        attendance.extend(student["attendance"].get(course, []))
+    return sum(attendance) / len(attendance) if attendance  else 0.0
+
+def school_average(data: Dict, school: str) -> float:
+    students: List[Student] = list(filter(lambda s: s['school'] == school, data['students']))
+    grades: List[int] = [grade for student in students for courses in student['grades'].values() for grade in courses]
+    return sum(grades) / len(grades) if grades else 0.0
+
+def school_attendance(data: Dict, school: str) -> float:
+    students: List[Student] = list(filter(lambda s: s["school"] == school, data["students"]))
+    attendance: List[int] = [a for s in students for c in s["attendance"].values() for a in c]
+    return sum(attendance) / len(attendance) if attendance else 0.0
 
 
-    def add_attendance(self, subject, presence):
-        if subject not in self.attendance:
-            self.attendance[subject] = []
-        self.attendance[subject].append(presence)
+def dataset() -> Dict:
+    courses = ['Mathematics', 'Chemistry', 'History', 'Art', 'Physics', 'Biology']
+    schools = ['Highschool A', 'Highschool B']
 
-    def calculate_average(self):
-        all_grades = []
-        for grades in self.grades.values():
-            all_grades.extend(grades)
-        if all_grades:
-            return sum(all_grades) / len(all_grades)
-        return 0
-
-    def calculate_attendance(self):
-        all_presences = []
-        for subject in self.attendance:
-            all_presences.extend(self.attendance[subject])
-        if all_presences:
-            return sum(all_presences) / len(all_presences)
-        return 0
-
-class Class(Student):
-    def __init__(self, subject):
-        self.subject = subject
-        self.students = []
-    
-    def add_student(self, student):
-        self.students.append(student)
-
-    def calcualte_class_average(self):
-        grades = []
-        for student in self.students:   
-            if self.subject in student.grades:
-                grades.extend(student.grades[self.subject])
-        if grades:
-            return sum(grades) / len(grades)
-        return 0
-
-    def calculate_class_attendance(self):
-        presences = []
-        for student in self.students:
-            if self.subject in student.attendance:
-                presences.extend(student.attendance[self.subject])
-        if presences:
-            return sum(presences) / len(presences)
-        return 0
-
-class HighSchool(Class):
-    def __init__(self, name):
-        self.name = name
-        self.classes = []
-    
-    def add_class(self, class_arg):
-        self.classes.append(class_arg)
-
-    def school_attendance(self):
-        presences = []
-        for class_arg in self.classes:
-            for student in class_arg.students:
-                if class_arg.subject in student.attendance:
-                    presences.extend(student.attendance[class_arg.subject])
-        if presences:
-            return sum(presences) / len(presences)
-        return 0
+    students = []
+    for i in range(20):
+        students.append({
+            "name": f"Student{i+1}",
+            "school": schools[i % 2],
+            "grades": {course: random.choices([3, 4, 5], k=3) for course in courses},
+            "attendance": {course: random.choices([1, 1, 0], k=3) for course in courses}
+        })
+    return {'students': students, 'courses': courses, 'schools': schools}
 
 if __name__ == "__main__":
-    student1 = Student("Natalia", "Kowalska")
-    student2 = Student("Weronika", "Bartoszewska")
-    student3 = Student("Alicja", "Jagiełło")
+    data = dataset()
+    save_data('school_data.json', data)
+    data = load_data('school_data.json')
 
-    python_class = Class("Python")
+    student = data["students"][0]
+    print("Student:", student["name"])
+    print("Total Average:", student_total_average(student))
 
-    python_class.add_student(student1)
-    python_class.add_student(student2)
-    python_class.add_student(student3)
+    print("Average in Mathematics:", student_couse_average(student, "Mathematics"))
+    print("Attendance in Mathematics:", student_attendance(student, "Mathematics"))
 
-    student1.add_grade("Python", 5)
-    student1.add_grade("Python", 4)
-    student1.add_attendance("Python", 1)
-    student1.add_attendance("Python", 1)    
-    student2.add_grade("Python", 3)
-    student2.add_grade("Python", 4)
-    student2.add_attendance("Python", 1)
-    student2.add_attendance("Python", 0)    
-    student3.add_grade("Python", 5)
-    student3.add_grade("Python", 5)
-    student3.add_attendance("Python", 1)
-
-    school = HighSchool("High School AGH")
-    school.add_class(python_class)
-
-    print(f"Average grade for {student1.name}: {student1.calculate_average()}")
-    print(f"Average attendance for {student1.name}: {student1.calculate_attendance()}")
-    print(f"Class average for Python: {python_class.calcualte_class_average()}")
-    print(f"Class attendance for Python: {python_class.calculate_class_attendance()}")
-    print(f"School attendance: {school.school_attendance()}")
-    
+    print("Average in Chemistry for all students:", course_average(data, "Chemistry"))
+    print("Average for Highschool A:", school_average(data, "Highschool A"))
+    print("Attendance for Highschool B:", school_attendance(data, "Highschool B"))
